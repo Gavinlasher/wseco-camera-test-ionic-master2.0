dfb598be25017bee12872e16ba62333555fc5a90
Gavin Lasher <Gavin.Lasher@wseco.com>
2022-05-16 16:46:52 -0600

    commit

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,80 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Vue Camera App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+     <ion-grid>
+    <ion-row>
+      <ion-col size="6" :key="photo" v-for="photo in photos">
+        <ion-img :src="photo.webviewPath"></ion-img>
+      </ion-col>
+    </ion-row>
+     </ion-grid>
+     <ion-fab vertical="bottom" horizontal="center" slot="fixed" >   
+<ion-fab-button @click="takePhoto()">
+<ion-icon :icon="camera"> 
+
+</ion-icon>
+</ion-fab-button>
+     </ion-fab>
+      
+     
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {camera,trash, close} from "ionicons/icons"
+import { defineComponent } from 'vue';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+import { setupConfig } from "@ionic/core";
+import  {usePhotoGallery, UserPhoto} from "@/composables/usePhotoGallery"
+
+
+
+export default {
+  name: 'Tab2Page',
+components: {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  },
+
+setup(){
+  const {takePhoto , photos} = usePhotoGallery();
+  return{
+camera, trash,close, takePhoto , photos
+  }
+}
+
+
+}
+
+
+
+</script>
